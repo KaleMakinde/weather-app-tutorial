3e9f9144d0a833b89d74490c9b25e8d5287725d7
KaleMakinde <makindeadeyemiadekale@gmail.com>
2023-07-14 21:10:24 +0100

    add message

--- src/views/SideView/WeatherSearch.jsx
+++ src/views/SideView/WeatherSearch.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Close from '../../assets/svgs/close';
+import Search from '../../assets/svgs/search';
+import './sideview.styles.css';
 
 const WeatherSearch = () => {
   return (
-    <div>
-      WeatherSearch
+    <div className='bg-[#1E213A] min-h-screen'>
+
+      <div className='search-container'>
+        <div className="close-btn"> <Close /></div>
+        
+        <form className="search-role">
+            <div className="search-btn"><Search /></div>
+
+                <input
+                type="search"
+                className="query-details"
+                 placeholder="search location"
+                 
+                 />
+        </form>
+
+       
+        
+        <button className='btn-search'>Search</button>
+        
+      </div>
     </div>
   )
 }
--- src/views/SideView/index.jsx
+++ src/views/SideView/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import CurrentWeather from './CurrentWeather'
+import WeatherSearch from './WeatherSearch'
 
 const SideView = () => {
   return (
-    <div>
+   
+   <div>
       <CurrentWeather/>
+      <WeatherSearch/> 
+
     </div>
   )
 }
